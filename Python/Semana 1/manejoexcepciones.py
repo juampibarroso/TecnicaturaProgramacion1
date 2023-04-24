70f3e3569bd147129483bb71a462973e118d5d28
Juan Pablo Barroso <108449487+juampibarroso@users.noreply.github.com>
2023-04-24 14:13:34 -0300

    Update manejoexcepciones.py

--- Python/Semana 1/manejoexcepciones.py
+++ Python/Semana 1/manejoexcepciones.py
@@ -1,15 +1,25 @@
+from NumerosIgualesException import NumerosIgualesException
+
 resultado = None
 
-try:
-    a = int(input('Digite el primer numero: '))
-    b = int(input('Digite el segundo numero: '))
-    resultado = a / b #modificamos
-except TypeError as e:
-    print(f'TypeError - Ocurrio un error: {type(e)}')
-except ZeroDivisionError as e:
-    print(f'ZeroDivisionError - Ocurrio un error: {type(e)}')
-except Exception as e:
-    print(f'Exception - Ocurrio un error: {type(e)}')
 
-print(f'El resultado es: {resultado}')
-print('seguimos...')
+
+try: 
+    a = int(input("Digite el primer numero: "))
+    b = int(input("Digite el segundo numero: ")) 
+    if a == b:
+        raise NumerosIgualesException("Son numeros Iguales")
+    resultado= a / b 
+except TypeError as e:
+    print(f"Ocurrio un eror Type Error: {type(e)}")
+except ZeroDivisionError as e:
+    print(f"Ocurrio un eror: {type(e)}")
+except Exception as e:
+    print(f"Ocurrio un error: {type(e)}")
+else:
+    print(f"No se arrojo ninguna exception")
+finally:
+    print(f"Ejecucion de este bloque finally")
+
+print(f"El resultado es: {resultado} ")
+print(f"Seguimos...")
