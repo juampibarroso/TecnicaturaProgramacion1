e3fe0c8acdc2c9f83137a781f5eea64da08e10ba
sidorukandres <111541513+sidorukandres@users.noreply.github.com>
2023-05-23 19:38:59 -0300

    Leccion05
    Agregue la leccion05

---
+++ Java/Leccion05/ConversionObjetos/src/domain/TipoEscritura.java
@@ -0,0 +1,21 @@
+
+package domain;
+
+
+public enum TipoEscritura {
+    CLASICO("Escritura a mano"),
+    MODERNO("Escritura digital");
+    
+    private final String descripcion;
+    
+    private TipoEscritura(String descripcion){ //Contructor
+        this.descripcion = descripcion;
+    }
+    
+    //Método get
+    public String getDescripcion(){
+        return this.descripcion;
+    }
+    
+    
+}
